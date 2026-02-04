60329f38bc7cfac12d87adf13800f7a133192584
Ishan <25ishansachin.acc@lwetb.ie>
2026-02-04 19:10:15 +0000

    Add files via upload

---
+++ is it an invalid month?.py
@@ -0,0 +1,20 @@
+year = int(input('enter the year: '))
+date = int(input('enter the date: '))
+month = int(input('enter the month: '))
+if (year < 0):
+    print('INVALID PLEASE RERUN')
+    if (month < 1):
+        print('INVALID PLEASE RERUN')
+        if (month >= 13):
+             print('INVALID PLEASE RERUN')
+             if (date < 1):
+                 print('INVALID PLEASE RERUN')
+                 if (date >= 31):
+                     print('INVALID PLEASE RERUN')
+             
+        
+
+    
+        
+     
+    
