a089459c489b3727f44794f34ef1ec886e254900
Ishan <25ishansachin.acc@lwetb.ie>
2026-02-11 15:36:07 +0000

    Add files via upload

---
+++ simple intrest calc.py
@@ -0,0 +1,5 @@
+P = float(input('enter the pricipal amount: '))
+N = float(input('enter the time in years: '))
+R = float(input('enter the annual rate of intrest: '))
+SI = (P * N * R) / 100
+print(SI)
---
+++ smallest of 2 numbers.py
@@ -0,0 +1,6 @@
+NUM1 = float(input('enter a number: '))
+NUM2 = float(input('enter another number: '))
+if NUM1 < NUM2:
+    print('smallest number is number 1 ')
+else:
+    print('smallest number is number 2')
